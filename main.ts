a562d7d787e43b9f00ad30b02801c57d338e3896
Thanh Pham Ngoc <thanh.phamngoc@nashtechglobal.com>
2023-09-06 15:32:48 +0700

    feat(): copy code from acheong08/rev-obsidian-sync-plugin

--- main.ts
+++ main.ts
@@ -1,81 +1,77 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import { App, Notice, Plugin, PluginManifest, PluginSettingTab, Setting } from 'obsidian';
+import type { BrowserWindow } from 'electron';
 
-// Remember to rename these classes and interfaces!
-
-interface MyPluginSettings {
-	mySetting: string;
+interface ReplicatePluginSettings {
+	syncBaseUrl: string;
 }
 
-const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
+const DEFAULT_SETTINGS: ReplicatePluginSettings = {
+	syncBaseUrl: 'https://api.obsidian.md'
 }
 
-export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings;
+export default class ReplicatePlugin extends Plugin {
+	settings: ReplicatePluginSettings;
+  private origGetHost: any;
+
+  constructor(app: App, manifest: PluginManifest) {
+		super(app, manifest);
+
+		const syncInstance = this.getInternalPluginInstance('sync');
+		this.origGetHost = syncInstance.getHost.bind(syncInstance);
+	}
 
 	async onload() {
 		await this.loadSettings();
 
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
-		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
+		// This adds a settings tab so the user can configure various aspects of the plugin
+		this.addSettingTab(new ReplicateSettingTab(this.app, this));
 
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
-
-		// This adds a simple command that can be triggered anywhere
-		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
-			}
-		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
+		try {
+			(<BrowserWindow>(
+        /*eslint @typescript-eslint/no-explicit-any: off */
+				(window as any).electronWindow
+			)).webContents.session.webRequest.onBeforeRequest(
+				{ urls: ['https://api.obsidian.md/*'] },
+				async ({ url }, callback) => {
+					await this.loadSettings();
+					// Replace api url with sync api url
+					if (url.startsWith('https://api.obsidian.md')) {
+						url = url.replace(
+							'https://api.obsidian.md',
+							this.settings.syncBaseUrl || DEFAULT_SETTINGS.syncBaseUrl
+						);
+					} else if (url.startsWith('https://publish.obsidian.md')) {
+						url = url.replace(
+							'https://publish.obsidian.md',
+							this.settings.syncBaseUrl ||
+								'https://publish.obsidian.md'
+						);
 					}
 
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
+					callback({ redirectURL: url });
 				}
+			);
+		} catch (e) {
+			new Notice('Failed to intercept requests. The error was: ' + e);
+		}
+
+		this.getInternalPluginInstance('sync').getHost = () => {
+			let url = this.origGetHost();
+			const syncBaseUrl = this.settings.syncBaseUrl;
+
+			if (syncBaseUrl) {
+				const scheme = syncBaseUrl.startsWith('http:') ? 'ws' : 'wss';
+				const syncBaseUrlWithoutScheme = syncBaseUrl.replace(
+					/^https?:\/\//,
+					''
+				);
+				url = `${scheme}://${syncBaseUrlWithoutScheme}/ws.obsidian.md`;
 			}
-		});
 
-		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
+			console.log('Websocket URL:', url);
 
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
-		});
-
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
+			return url;
+		};
 	}
 
 	onunload() {
@@ -89,28 +85,17 @@ export default class MyPlugin extends Plugin {
 	async saveSettings() {
 		await this.saveData(this.settings);
 	}
-}
 
-class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
-	}
-
-	onOpen() {
-		const {contentEl} = this;
-		contentEl.setText('Woah!');
-	}
-
-	onClose() {
-		const {contentEl} = this;
-		contentEl.empty();
+  getInternalPluginInstance(id: string) {
+		// @ts-ignore: Property 'internalPlugins' does not exist on type 'App'.
+		return this.app.internalPlugins.getPluginById(id).instance;
 	}
 }
 
-class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
+class ReplicateSettingTab extends PluginSettingTab {
+	plugin: ReplicatePlugin;
 
-	constructor(app: App, plugin: MyPlugin) {
+	constructor(app: App, plugin: ReplicatePlugin) {
 		super(app, plugin);
 		this.plugin = plugin;
 	}
@@ -121,13 +106,13 @@ class SampleSettingTab extends PluginSettingTab {
 		containerEl.empty();
 
 		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
+			.setName('Sync Base Url')
+			.setDesc('It\'s a sync base url')
 			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
+				.setPlaceholder('Enter your sync base url')
+				.setValue(this.plugin.settings.syncBaseUrl)
 				.onChange(async (value) => {
-					this.plugin.settings.mySetting = value;
+					this.plugin.settings.syncBaseUrl = value;
 					await this.plugin.saveSettings();
 				}));
 	}
